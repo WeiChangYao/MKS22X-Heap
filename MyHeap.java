970d13192c0b2e1c16e5eebb74d9a25d82198a25
weichangyao <wyao1@stuy.edu>
2019-04-29 11:44:01 -0400

    added swap

---
+++ MyHeap.java
@@ -0,0 +1,13 @@
+public class MyHeap{
+  private static void swap(int[]data, int indexOne, int indexTwo){
+    int swapdata = data[indexOne];
+    data[indexOne] = data[indexTwo];
+    data[indexTwo] = swapdata;
+  }
+  private static void pushDown(int[]data,int size,int index){
+
+  }
+  private static void pushUp(int[]data,int index){}
+  public static void heapify(int[]){}
+  public static void heapsort(int[]){}
+}
