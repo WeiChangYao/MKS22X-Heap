609e495c36ff5a46a414aa633f542095162901f1
weichangyao <wyao1@stuy.edu>
2019-04-30 11:44:34 -0400

    tested my swap

--- MyHeap.java
+++ MyHeap.java
@@ -10,11 +10,17 @@ public class MyHeap{
   private static void pushUp(int[]data,int index){}
   public static void heapify(int[]data){}
   public static void heapsort(int[]data){}
-
+  public static String prin(int[] ary){
+    String s = "";
+    for (int i = 0; i < ary.length; i++){
+      s+= ary[i];
+    }
+    return s;
+  }
   public static void main(String[] args){
     int[] cat = new int[]{1,2,3};
-    System.out.println(cat);
+    System.out.println(prin(cat));
     swap(cat,1,2);
-    System.out.println(cat);
+    System.out.println(prin(cat));
   }
 }
