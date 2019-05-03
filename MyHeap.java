861d59b1f18d1f3190131b5cc86834d36e2ce515
wyao1 <wyao1@stuy.edu>
2019-05-03 03:34:46 -0400

    added pushDown Method

--- MyHeap.java
+++ MyHeap.java
@@ -5,31 +5,37 @@ public class MyHeap{
     data[indexTwo] = swapdata;
   }
   private static void pushDown(int[]data,int size,int index){
-    //why do i need size? to over
-    if(data[index] < data[index*2+1]){
-      swap(data,index,index*2+1);
-      //recursive?
-      pushDown(data,size,index*2+1)
-    }
-    if(data[index] < data[index*2+2]){
-      swap(data,index,index*2+2);
-    }
-
+    if (index >= (size/2) && index <= size) { 
+      return; 
+    } 
+    if (data[index] < data[index*2] ||  
+        data[index] < data[index*2+1]) { 
+        if (data[index*2] > data[index*2+1]) { 
+           swap(data, index, index*2);
+           pushDown(data,size,index*2); 
+        } 
+        else { 
+           swap(data, index, index*2+1); 
+           pushDown(data,size,index*2+1);
+        } 
+    } 
   }
+  
   private static void pushUp(int[]data,int index){}
   public static void heapify(int[]data){}
   public static void heapsort(int[]data){}
+  
   public static String prin(int[] ary){
     String s = "";
     for (int i = 0; i < ary.length; i++){
-      s+= ary[i];
+      s+= ary[i] + " ";
     }
     return s;
   }
   public static void main(String[] args){
-    int[] cat = new int[]{1,2,3};
+    int[] cat = new int[]{3,10,2,5,4,8};
     System.out.println(prin(cat));
-    swap(cat,1,2);
+    pushDown(cat,6,2);
     System.out.println(prin(cat));
   }
 }
