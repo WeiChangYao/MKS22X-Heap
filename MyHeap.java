5154e695deded8ff7eaf70238aafcdafbf4fa196
wyao1 <wyao1@stuy.edu>
2019-05-03 03:48:46 -0400

    fixed push down

--- MyHeap.java
+++ MyHeap.java
@@ -7,20 +7,31 @@ public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
     if (index >= (size/2) && index <= size) { 
       return; 
-    } 
-    if (data[index] < data[index*2] ||  
-        data[index] < data[index*2+1]) { 
-        if (data[index*2] > data[index*2+1]) { 
-           swap(data, index, index*2);
-           pushDown(data,size,index*2); 
+    }
+    //if(size%2==0){
+    
+    if (data[index] < data[index*2+1] ||  
+        data[index] < data[index*2+2]) { 
+        if (data[index*2+1] > data[index*2+2]) { 
+           swap(data, index, index*2+1);
+           pushDown(data,size,index*2+1); 
         } 
         else { 
-           swap(data, index, index*2+1); 
-           pushDown(data,size,index*2+1);
+           swap(data, index, index*2+2); 
+           pushDown(data,size,index*2+2);
         } 
     } 
+    
+    //}
+    /*else{
+      if(index*2+1 < size){
+      if (data[index] < data[index*2+1]){
+        swap(data, index, index*2+1);
+        pushDown(data,size,index*2+1); 
+      }
+    }
+    }*/
   }
-  
   private static void pushUp(int[]data,int index){}
   public static void heapify(int[]data){}
   public static void heapsort(int[]data){}
@@ -33,9 +44,9 @@ public class MyHeap{
     return s;
   }
   public static void main(String[] args){
-    int[] cat = new int[]{3,10,2,5,4,8};
+    int[] cat = new int[]{3,8,2,5,10};
     System.out.println(prin(cat));
-    pushDown(cat,6,2);
+    pushDown(cat,5,1);
     System.out.println(prin(cat));
   }
 }
